1f81f644789606dab41d1a6835dd0a6b6a2a2191
Your Name <ramusarithak@gmail.com>
2019-11-09 12:15:43 +0530

    Eamilpattern

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component ,ChangeDetectorRef,ElementRef,ViewChild} from '@angular/core';
-import {FormBuilder,FormArray} from '@angular/forms'
+import { Component, ChangeDetectorRef, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormArray, FormGroup, Validators, PatternValidator } from '@angular/forms'
 
 import { from } from 'rxjs';
 
@@ -9,43 +9,45 @@ import { from } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'formControl';
-
-
-// city select
-City:any=['F','dfsdfds','dfsfd','dfsfdsfgreger']
-registrationForm = this.fb.group({
-  address:this.fb.group({
-    city:['']
-  })
-})
-
-changeCity(e){
-  this.registrationFor.get('address.cityName').setValue(e.target.value,{
-    onlySelf:true
-  })
-}
-
-
-// 2 start
+  FormBuilder: any;
   constructor(
     public fb: FormBuilder,
     private cd: ChangeDetectorRef
-  ) {}
 
-  // registrationForm = this.fb.group({
-  //   file: [null]
-  
-  // })  
+  ) { }
 
-
-  @ViewChild('fileInput',{static:false}) el: ElementRef;
+@ViewChild('fileInput', { static: false })  el: ElementRef;
   imageUrl: any = '/assets/ir.png';
   editFile: boolean = true;
   removeUpload: boolean = false;
+  // city select
+  City: any = ['Tamilnadu', 'Delhi', 'Mumbai', 'AndraPradesh']
+  registrationForm = this.fb.group({
+    address: this.fb.group({
+      city: ['']
+    })
+  })
+
+  changeCity(e) {
+    this.registrationFor.get('address.cityName').setValue(e.target.value, {
+      onlySelf: true
+    })
+  }
+
+
+  // 2 start
+
+
+  // registrationForm = this.fb.group({
+  //   file: [null]
+
+  // })  
+
+
+  
 
   uploadFile(event) {
-    let reader = new FileReader(); 
+    let reader = new FileReader();
     let file = event.target.files[0];
     if (event.target.files && event.target.files[0]) {
       reader.readAsDataURL(file);
@@ -60,7 +62,7 @@ changeCity(e){
         this.removeUpload = true;
       }
 
-      this.cd.markForCheck();        
+      this.cd.markForCheck();
     }
   }
 
@@ -76,33 +78,42 @@ changeCity(e){
 
 
   // 2 end
-  
+
 
   // 1 start
 
   // constructor(public fb:FormBuilder){}
-  registrationFor=this.fb.group({
-    file:[null],
-    fullname:this.fb.group({
-      firstname:[''],
-      lastname:['']
-       }   ),
+  registrationFor = this.fb.group({
+    file: [null],
+    fullname: this.fb.group({
+      firstname: [''],
+      lastname: ['']
+    }),
 
-       email:[''],
-       phoneNumber:[''],
-       address:this.fb.group({
-         street:[''],
-         city:[''],
-         cityName:['']
-       }),
-       gender:[''],
-       passwordValidation:this.fb.group({
-         password:[''],
-         confirmPassword:['']
-       }),
-       addDynamicElements:this.fb.array([])
+    email: [''],
+    phoneNumber: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      cityName: ['']
+    }),
+    gender: [''],
+    passwordValidation: this.fb.group({
+      password: [''],
+      confirmPassword: ['']
+    }),
+    addDynamicElements: this.fb.array([])
   });
 
-
+  onSubmit() {
+    console.log("your form submited successfully");
+    let emailRegEx = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    this.registrationForm=this.FormBuilder.group({
+      
+      Name:['',Validators.required],
+      Email:['',Validators.required,Validators.pattern(emailRegEx)],
+      Password:['',Validators.required],
+      ConfirmPassword:['',Validators.required]
+    })
+  }
 }
-  
